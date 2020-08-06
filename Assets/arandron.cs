85d4193c8339f9205a3aeab7cd53c6d053e408cd
Asato <50614156+asatotaiki@users.noreply.github.com>
2020-08-06 11:15:42 +0900

    エラー時のビルド

---
+++ Assets/arandron.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arandron : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject gun;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gunshot();
+    }
+    public void gunshot()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+            GameObject BT = (GameObject)Instantiate(bullet, transform.position.normalized, Quaternion.Euler(transform.parent.eulerAngles.x, transform.parent.eulerAngles.y, 0));
+            Rigidbody RB = BT.GetComponent<Rigidbody>();
+            RB.AddForce(transform.forward * speed);
+            Destroy(BT, 5.0f);
+
+        }
+    }
+    public void gunshotposition()
+    {
+        this.transform.position = gun.transform.position;
+    }
+}
